ce714f0b87cf00050cdf3c3029ba4d5a36a96402
kevinmilet <kevin.milet@gmail.com>
2020-11-02 09:43:36 +0100

    exo 1, 2 et 3 ok

--- src/exercicesAnswers.js
+++ src/exercicesAnswers.js
@@ -1,21 +1,26 @@
 //Exercice 1 : Créer un tableau days et lui ajouter les jours de la semaine.
 let createArray = function () {
-  return 'A compléter';
+  let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  return days;
 }
 
 //Exercice 2 : Retouner le 3ème élément du tableau days', function() {
 let returnThirdElement = function (array) {
-  return 'A compléter';
+  let reponse = array[2];
+  return reponse;
 }
 
 //Exercice 3 : Afficher la valeur de l\'index 3 du tableau days.', function() {
 let returnThirdIndex = function (array) {
-  return 'A compléter';
+  let reponse = array[3];
+  return reponse;
 }
 
 //Exercice 4 : Changer le 2ème élément du tableau languages par php.', function() {
 let changeSecondElement = function (array) {
-  return 'A compléter';
+  let reponse = array.splice(1, 1, "php");
+  console.log(array);
+  return reponse;
 }
 
 //Exercice 5 : Ajouter ruby et python à la fin du tableau languages.', function() {
